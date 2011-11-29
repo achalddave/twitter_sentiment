a6b3f6b03b5508d1c53ce2c573bc5da3efe208c0
Akihiro Matsukawa <akihiro.matsukawa@gmail.com>
2011-11-28 16:48:46 -0800

    Sanitizer, feature selector

---
+++ Vector.py
@@ -0,0 +1,36 @@
+"""
+This borrows a lot from http://inst.eecs.berkeley.edu/~cs188/fa11/projects/search/docs/util.html
+This is currently incomplete. Will add functionality as needed.
+"""
+
+
+class Vector(dict):
+
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+
+    def __getitem__(self, key):
+        if key in self:
+            return dict.__getitem__(self, key)
+        else:
+            return 0
+
+    def normalize(self):
+        total = self.total_value() * 1.0
+        normalized = Vector()
+        for k in self:
+            normalized[k] = self[k]/total
+        return normalized
+
+    def total_value(self):
+        return sum(self.values())
+
+    def arg_min(self):
+        if not self:
+            return None
+        return sorted(self.items(), key=lambda x: x[1])[0][0]
+        
+    def arg_max(self):
+        if not self:
+            return None
+        return sorted(self.items(), key=lambda x: -x[1])[0][0]

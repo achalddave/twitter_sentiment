c7a6bfc8f47c746cef989cda693b67bc1a8d6d50
Sharad Vikram <sharad.vikram@gmail.com>
2013-02-06 01:31:42 -0800

    updated to not be a submodule

--- get_tweets.py
+++ get_tweets.py
@@ -1,11 +1,11 @@
 import sys
-sys.path.insert(0, 'python-twitter/')
+sys.path.insert(0, 'twitter/')
 
 import twitter
 
-api = twitter.Api(consumer_key="ElptdK9AGjxOBuORbiuTQ", 
-    consumer_secret="iufJBk1qhX84VVeCeeqeOJrVgDkqmtiVobNTpMKUA", 
-    access_token_key="370357185-yFfM8KRTOLuQJqfaURThsTN53clQK5oxAyTbvmb0", 
+api = twitter.Api(consumer_key="ElptdK9AGjxOBuORbiuTQ",
+    consumer_secret="iufJBk1qhX84VVeCeeqeOJrVgDkqmtiVobNTpMKUA",
+    access_token_key="370357185-yFfM8KRTOLuQJqfaURThsTN53clQK5oxAyTbvmb0",
     access_token_secret="1nSEOq2a6STHJm4gIVObMI0bkoL6cG8zH7n0qQ2bKc")
 
 def get_tweets():
